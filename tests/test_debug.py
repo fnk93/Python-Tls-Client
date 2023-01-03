4169289724f6418c1250452d5aa6f6328aeaa85f
fnk93 <f.krueger1993@gmail.com>
2023-01-03 18:41:29 +0100

    tests: Add session test with debug turned on.

---
+++ tests/test_debug.py
@@ -0,0 +1,22 @@
+import tls_client
+
+
+def test_cookies():
+    client_identifier = "chrome_107"
+    session = tls_client.Session(
+        client_identifier=client_identifier,
+        random_tls_extension_order=True,
+        debug=True,
+    )
+    print(session.get('https://httpbin.org/cookies/set/testcookie/12345'))
+    print(session.get('https://httpbin.org/cookies').json())
+    print(session.cookies.get_dict())
+    print(session.get_cookies(url='https://httpbin.org'))
+    print(session.clear_cookies())
+    print(session.cookies.get_dict())
+    print(session.get_cookies(url='https://httpbin.org'))
+    print(session.get('https://httpbin.org/cookies').json())
+
+
+if __name__ == '__main__':
+    test_cookies()
