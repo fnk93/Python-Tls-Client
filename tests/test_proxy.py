20f334e8ddff78fccaf7888b0d0423fe0c8cc9e1
fnk93 <f.krueger1993@gmail.com>
2023-01-10 00:14:05 +0100

    test: proxy-test case

---
+++ tests/test_proxy.py
@@ -0,0 +1,17 @@
+import tls_client
+
+
+def test_proxy_on_get():
+    client_identifier = "chrome_107"
+    session = tls_client.Session(
+        client_identifier=client_identifier,
+        random_tls_extension_order=True,
+        debug=True,
+    )
+    resp = session.get('https://api.ipify.org/?format=json', proxy="http://45.8.179.241:1337")
+    print(resp.json())
+    session.close()
+
+
+if __name__ == '__main__':
+    test_proxy_on_get()
